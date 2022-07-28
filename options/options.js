065e44c1d1431614092b126fe3e02b90c6b9a1ce
Crowdbotics <team@crowdbotics.com>
2022-07-28 07:00:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cash_gold_pro_36452",
+  url: "https://cash_gold_pro_36452.botics.co",
+  api: "https://cash_gold_pro_36452.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
